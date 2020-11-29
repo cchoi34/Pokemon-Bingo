75eb7482f91a8930b63bc4f76e313b4d3ffa1332
Chris Choi <christopherchoi97@gmail.com>
2020-11-29 17:28:28 -0600

    adding firebase init

--- pokemon-bingo/src/index.js
+++ pokemon-bingo/src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDn0w9mkuA2a_MuNBEzeY4TcHPs1dDqeK8",
+  authDomain: "pokemon-bingo-1f8dc.firebaseapp.com",
+  databaseURL: "https://pokemon-bingo-1f8dc.firebaseio.com",
+  projectId: "pokemon-bingo-1f8dc",
+  storageBucket: "pokemon-bingo-1f8dc.appspot.com",
+  messagingSenderId: "836921431846",
+  appId: "1:836921431846:web:bc79adc0fd8903e5d213e7",
+  measurementId: "G-ZVME4NCQ92"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
